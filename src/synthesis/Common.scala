231ddf202630e01502560c404f9989a7c0ff9cb4
Mikaël Mayer <mikael.mayer@polytechnique.org>
2009-11-12 18:41:15 +0000

    Made the Bezout function work.

--- src/synthesis/Common.scala
+++ src/synthesis/Common.scala
@@ -4,7 +4,7 @@ object Common {
   
   def bezout(e: Int, a : Int*): List[Int] = bezout(e, a.toList)
   
-  // Finds a vector x such that x.a + e is the closest as possible to 0, and postive if there are two possibilities.
+  // Finds a vector x such that x.a + e is 0 if this is possible.
   def bezout(e: Int, a : List[Int]):List[Int] = {
     a match {
       case Nil => Nil
@@ -52,7 +52,7 @@ object Common {
                     l.splitAt(index) match {
                     // The coefficient at index 'index' of l should be zero, we replace it by its assignment
                     case (left, 0::right) =>
-                      val solution_a = ((a_assign zip q).foldLeft(0){ case (result, (k, l)) => result + k*l})
+                      val solution_a = scalarProduct(a_assign, q)
                       val solution = (solution_a + e_assign)*(min_coef_sign)
                       val result = left++(solution::right)
                       result
@@ -62,9 +62,138 @@ object Common {
             }
         }
     }
-    
+  }
+
+  def enumerate[A](a: List[A]):List[(Int, A)] = (a.indices zip a)
+  
+  // a is an indexed list
+  def replaceElementAtIndexByCoef(a: List[(Int, Int)], index: Int, coef: Int) = a map { _ match {
+        case (i, c) =>  if(i == index) coef else c
+    } }
+  
+  // a is an indexed list
+  def replaceEverythingByCoef1ExceptCoef2AtIndex(a: List[(Int, Int)], coef1:Int, coef2: Int,index: Int): List[Int] =
+    a map { _ match {
+        case (i, c) if i == index => coef2
+        case _ => coef1
+      }
+    }
+  
+  // a is an indexed list
+  def replaceEverythingByZeroExcept1AtIndex(a: List[(Int, Int)], index: Int): List[Int] =
+    replaceEverythingByCoef1ExceptCoef2AtIndex(a, 0, 1, index)
+
+  // a is an indexed list
+  def putCoef1AtIndex1andCoef2AtIndex2(a: List[(Int, Int)], coef1: Int, index1: Int, coef2: Int, index2: Int ):List[Int] = {
+    a map { _ match {
+        case (i, c) =>
+          if(i == index1) coef1
+          else if(i==index2) coef2
+          else 0
+      }
+    }
   }
   
+  //insertPreviousZeros([a, b, 0, c, 0, d], [x, y, z, w]) = [x, y, 0, z, 0, d]
+  def insertPreviousZeros(list_with_zeros: List[Int], list_to_complete: List[Int]) :List[Int] = {
+    val (_, result) = list_with_zeros.foldLeft((list_to_complete, Nil: List[Int]))( (_, _) match {
+      case ((l, result), 0)    => (l, 0::result)
+      case ((a::q, result), _) => (q, a::result)
+    })
+    result.reverse
+  }
+  
+  def scalarProduct(a: List[Int], b:List[Int]): Int = {
+    (a zip b).foldLeft(0){case (result, (e_a, e_b)) => result + e_a * e_b}
+  }
+  
+  def bezoutWithBase(e: Int, a : Int*): List[List[Int]] = bezoutWithBase(e, a.toList)
+  // If size(a) == n, finds a matrix x of size n x n
+  // such that (1, k1, k2, .. kn-1).x.a + e = 0 if this is possible,
+  // and x describes all integer solutions of this equation. Rank(x) = n-1
+  // Output representation : it's the list of rows.
+  //                               n          n     n
+  def bezoutWithBase(e: Int, a: List[Int]): (List[List[Int]]) = {
+    a match {
+      case Nil => Nil
+      case 0::Nil => ((0::Nil)::Nil)
+      case k::Nil => (((-e + smod(e, k))/k) :: Nil) :: Nil
+      case a =>
+        val a_indexed:List[(Int, Int)] = enumerate(a)
+        (a_indexed find (_._2 == 0)) match {
+          case Some((index, element)) =>
+          // Takes the result when removing the zeros, then inserts some zeros.
+            val a_filtered = a remove ( _ == 0 )
+            val solution = bezoutWithBase(e, a_filtered)
+            val (_, solution_completed) = a_indexed.foldLeft((solution, Nil:List[List[Int]]))((_,_) match { 
+              case ((q, l), (i, 0))    => (q, replaceEverythingByZeroExcept1AtIndex(a_indexed, i)::l)
+              case ((Nil, l), (i, k))  => (Nil, l) // This should not happen, as we have many zeroes.
+              case ((b::q, l), (i, k)) => (q, insertPreviousZeros(a, b)::l)
+            })
+            // For each zero in a_indexed, add a new vector where 1 is at this position and it's zero everywhere else.
+            val new_vectors = a_indexed flatMap {
+              case (index, 0) => List(replaceEverythingByZeroExcept1AtIndex(a_indexed, index))
+              case t => Nil
+            }
+            solution_completed.reverse
+          case None =>
+            (a_indexed find {case a => Math.abs(a._2) == 1}) match {
+              case Some((index, element)) =>
+                // This is easy, like solving 4x+y+45z=30, just say that y=30 and other are zero
+                // For the vectors, just take the other variables as themselves.
+                val neg_sign = if(element > 0) -1 else 1
+                replaceEverythingByCoef1ExceptCoef2AtIndex(a_indexed, 0, neg_sign*e, index) ::
+                (a_indexed flatMap ( _ match {
+                  case (i, c) =>
+                    if(i == index)
+                      Nil
+                    else
+                      List(putCoef1AtIndex1andCoef2AtIndex2(a_indexed, 1, i, neg_sign*c, index))
+                }))
+              case None =>
+                    val (index, min_coef) = a_indexed reduceLeft {
+                  (_, _) match {
+                    case (t1@(i, ci), t2@(j, cj)) => if(Math.abs(ci) < Math.abs(cj)) t1 else t2
+                  }}
+                val min_coef_sign = if(min_coef > 0) 1 else -1
+                val abs_min_coef_plus_1 = Math.abs(min_coef) + 1
+                val e_assign = smod(e, abs_min_coef_plus_1)
+                val a_assign = abs_min_coef_plus_1 :: (a map {case k => smod(k, abs_min_coef_plus_1)})
+                // the coefficient at index 'index+1' is now 1 or -1.
+                
+                val new_e = (e + min_coef * (smod(e, abs_min_coef_plus_1)))/abs_min_coef_plus_1
+                val new_a = min_coef::(a map {
+                  case k => (k + min_coef * (smod(k, abs_min_coef_plus_1)))/abs_min_coef_plus_1
+                })
+                // the coefficient at index 'index+1' is now 0, so it will be removed.
+                
+                // Now, there is at least one zero in new_a
+                bezoutWithBase(new_e, new_a) match {
+                  case Nil => throw new Error("Should not happen as the input was not empty")
+                  case v =>
+                    val v_reduced = enumerate(v) flatMap {
+                      case (i, Nil) => throw new Error("Should not happen as the input was not empty")
+                      case (i, vs@(a::q)) =>
+                        if(i == index +1) // We drop the line indicated by index
+                          Nil
+                        else {
+                          val neg_sign = if(min_coef > 0) -1 else 1
+                          val vs_replaced = replaceElementAtIndexByCoef(enumerate(vs), index+1, 0)
+                          if(i==0) {
+                            val new_element = (e_assign+scalarProduct(a_assign, vs_replaced)) * min_coef_sign
+                            List(replaceElementAtIndexByCoef(enumerate(q), index, new_element))
+                          } else {
+                            val new_element = (scalarProduct(a_assign, vs_replaced)) * min_coef_sign
+                            List(replaceElementAtIndexByCoef(enumerate(q), index, new_element))
+                          }
+                        }
+                    }
+                    v_reduced
+                  }
+                }
+            }
+        }
+    }
   
   def smod(x:Int, m:Int) = {
     val c = x % m
@@ -96,7 +225,7 @@ object Common {
     case a::Nil => a
     case (a::b::q) => lcmlist(lcm(a,b)::q)
   }
-  // Computes the GCD over a list of numbers
+  // Computes the GCD over a list  of numbers
   def gcdlist(l:List[Int]):Int = l match {
     case Nil => 1
     case 0::Nil => 1
--- src/synthesis/CommonTest.scala
+++ src/synthesis/CommonTest.scala
@@ -4,8 +4,29 @@ import org.scalatest._
 import org.scalatest.matchers._
 
 class CommonTest extends Spec with ShouldMatchers {
+  describe("Auxiliary functions") {
+    it("should insert zeros properly") {
+      Common.insertPreviousZeros(List(-5, -6, 0, 3, 0, 2), List(17, -42, -53, 28)) should equal (List(17, -42, 0, -53, 0, 28))
+    }
+    it("should compute scalar product properly") {
+      Common.scalarProduct(List(3, 6, -2), List(5, -6, 4)) should equal (3*5+6*(-6)+4*(-2))
+    }
+    it("should replaceEverythingByCoef1ExceptCoef2AtIndex properly") {
+      Common.replaceEverythingByCoef1ExceptCoef2AtIndex(List((0, 1), (1, 5), (2, 4), (3, 6)), 3, 4, 2) should equal (List(3, 3, 4, 3))
+    }
+    it("should replaceEverythingByZeroExcept1AtIndex properly") {
+      Common.replaceEverythingByZeroExcept1AtIndex(List((0, 1), (1, 5), (2, 4), (3, 6)), 2) should equal (List(0, 0, 1, 0))
+    }
+    it("should putCoef1AtIndex1andCoef2AtIndex2 properly") {
+      Common.putCoef1AtIndex1andCoef2AtIndex2(List((0, 1), (1, 5), (2, 4), (3, 6)), 3, 1, 2, 3) should equal (List(0, 3, 0, 2))
+    }
+    it("should replaceElementAtIndexByCoef properly") {
+      Common.replaceElementAtIndexByCoef(List((0, 1), (1, 5), (2, 4), (3, 6)), 2, 1) should equal (List(1, 5, 1, 6))
+    }
+  }
+  
   describe("bezout function") {
-    it("should compute the simple bezout identity") {
+    it("should solve the simple bezout identity") {
       Common.bezout(6) should equal (Nil)
       Common.bezout(6, 3) should equal (List(-2))
       Common.bezout(-18, 6) should equal (List(3))
@@ -35,6 +56,35 @@ class CommonTest extends Spec with ShouldMatchers {
       Common.bezout(1973, 2*3*5,2*3*7,2*5*7,3*5*7) match {
         case x::y::z::w::Nil => (1973+x*2*3*5+y*2*3*7+z*2*5*7+w*3*5*7) should equal (0)
         case l => l should equal ("a list with four elements")
+      }
+    }
+    it("should solve the more complex bezout identity") {
+      Common.bezoutWithBase(6) should equal (Nil)
+      Common.bezoutWithBase(6, 3) should equal (List(List(-2)))
+      Common.bezoutWithBase(-18, 6) should equal (List(List(3)))
+      Common.bezoutWithBase(-7, 3) should equal (List(List(2)))
+      Common.bezoutWithBase(-8, 3) should equal (List(List(3)))
+      Common.bezoutWithBase(-13, 7) should equal (List(List(2)))
+      Common.bezoutWithBase(-11, 8) should equal (List(List(1)))
+      Common.bezoutWithBase(-12, 8) should equal (List(List(1)))
+      Common.bezoutWithBase(-13, 8) should equal (List(List(2)))
+      Common.bezoutWithBase(-13, 8, 0) should equal (List(List(2, 0), List(0, 1)))
+      Common.bezoutWithBase(-13, 42, 1, 17) should equal (List(List(0, 13, 0), List(1, -42, 0), List(0, -17, 1)))
+      Common.bezoutWithBase(5, 6, 4, 3) match {
+        case l@((a1::a2::a3::Nil)::(b1::b2::b3::Nil)::(c1::c2::c3::Nil)::Nil) =>
+          println(l)
+          (5+a1*6+a2*4+a3*3) should equal (0)
+          (b1*6+b2*4+b3*3) should equal (0)
+          (c1*6+c2*4+c3*3) should equal (0)
+        case l => l should equal ("a matrix with nine elements")
+      }
+      Common.bezoutWithBase(17, 10, 15, 6) match {
+        case l@((a1::a2::a3::Nil)::(b1::b2::b3::Nil)::(c1::c2::c3::Nil)::Nil) =>
+          println(l)
+          (17+a1*10+a2*15+a3*6) should equal (0)
+          (b1*10+b2*15+b3*6) should equal (0)
+          (c1*10+c2*15+c3*6) should equal (0)
+        case l => l should equal ("a matrix with nine elements")
       }
     }
   }
