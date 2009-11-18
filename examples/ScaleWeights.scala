44f217706ffd01ee7283262e3f089c89e3c2b075
Viktor Kuncak <viktor.kuncak@epfl.ch>
2009-11-18 18:15:10 +0000

    removed accidantal change to file

--- examples/ScaleWeights.scala
+++ examples/ScaleWeights.scala
@@ -7,14 +7,12 @@ object ScaleWeights {
 
     try {
       val (w1,w2,w3,w4) = choose((w1:Int,w2:Int,w3:Int,w4:Int) => (
-	!(0 <= weight && weight <= 39) ||
-        (
            w1 + 3 * w2 + 9 * w3 + 27 * w4 == weight
         && -1 <= w1 && w1 <= 1
         && -1 <= w2 && w2 <= 1
         && -1 <= w3 && w3 <= 1
         && -1 <= w4 && w4 <= 1
-      )))
+      ))
       println("Put what you think weights " + weight + " to the left, then")
       println("Put 1         : " + numToPlace(w1))
       println("Put 3         : " + numToPlace(w2))
