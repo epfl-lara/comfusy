a29f4749f1220c549f41ee6148936a544376fba8
Viktor Kuncak <viktor.kuncak@epfl.ch>
2009-11-20 19:44:50 +0000

    fourty two

---
+++ examples/FourtyTwo.scala
@@ -0,0 +1,12 @@
+import synthesis.Definitions._
+
+object FourtyTwo {
+  def main(args: Array[String]): Unit = {
+    println("Please enter a number: ")
+    val z: Int = Console.readInt
+
+    val 42 * x + 5 * y = z
+
+    println(z + " = 42 * " + x + " 5 * " + y)
+  }
+}
