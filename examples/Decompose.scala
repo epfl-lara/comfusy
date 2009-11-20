0bf86719f1c199c7f44689aa024389ba2dbffcd2
Philippe Suter <philippe.suter@gmail.com>
2009-11-20 12:51:40 +0000

    [empty]

--- examples/Decompose.scala
+++ examples/Decompose.scala
@@ -5,8 +5,8 @@ object Decompose {
     Console.println("Give me a number, please.")
     val n = Console.readInt
 
-    val 2 * x + 3 * y = n
+    val 2 * x - 5 * y = n
 
-    println("Did you know that " + n + " can be written as 2*" + x + " + 3*" + y + " ?")
+    println("Did you know that " + n + " can be written as 2*" + x + " - 5*" + y + " ?")
   }
 }
