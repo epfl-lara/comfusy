2d63fe1c75bf747f4f914943e4b0e957d80d5368
Paul Phillips <paulp@improving.org>
2012-12-18 20:29:23 -0800

    Everything in its place

--- project/Build.scala
+++ project/Build.scala
@@ -3,14 +3,14 @@ import Keys._
 
 object Comfusy extends Build {
   val commonSettings = Seq(
+    name := "comfusy",
+    organization := "org.improving",
     scalaVersion := "2.10.0-RC5",
     scalaBinaryVersion := "2.10.0-RC5",
-    organization := "org.improving",
     version := "1.0.0",
-    scalacOptions ++= Seq("-deprecation", "-unchecked")
+    scalacOptions ++= Seq("-deprecation", "-unchecked"),
+    libraryDependencies <+= (scalaBinaryVersion)("org.scala-lang" % "scala-compiler" % _)
   )
 
-  lazy val comfusy = Project(id = "comfusy", base = file(".")).settings(commonSettings: _*) aggregate(synthesis)
-  lazy val synthesis  = Project(id = "synthesis", base = file("synthesis")).settings(commonSettings: _*)
-  // lazy val decision  = Project(id = "decision", base = file("decision")).settings(commonSettings: _*) dependsOn synthesis
+  lazy val comfusy  = Project(id = "comfusy", base = file(".")).settings(commonSettings: _*)
 }
