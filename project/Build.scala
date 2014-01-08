e86aaa68c158c362c10b394cdc10c809e29808c7
Paul Phillips <paulp@improving.org>
2014-01-08 11:50:56 -0800

    scala 2.10.3, scalatest 2.0

--- project/Build.scala
+++ project/Build.scala
@@ -3,13 +3,14 @@ import Keys._
 
 object Comfusy extends Build {
   val commonSettings = Seq(
-    name := "comfusy",
-    organization := "org.improving",
-    scalaVersion := "2.10.0-RC5",
-    scalaBinaryVersion := "2.10.0-RC5",
-    version := "1.0.0",
-    scalacOptions ++= Seq("-deprecation", "-unchecked"),
-    libraryDependencies <+= (scalaBinaryVersion)("org.scala-lang" % "scala-compiler" % _)
+    name                 := "comfusy",
+    organization         := "org.improving",
+    scalaVersion         := "2.10.3",
+    // scalaBinaryVersion   := "2.10.3",
+    version              := "1.0.0",
+    scalacOptions       ++= Seq("-deprecation", "-unchecked"),
+    libraryDependencies  += "org.scala-lang" % "scala-compiler" % scalaVersion.value,
+    libraryDependencies  += "org.scalatest" %% "scalatest" % "2.0" % "test"
   )
 
   lazy val comfusy  = Project(id = "comfusy", base = file(".")).settings(commonSettings: _*)
